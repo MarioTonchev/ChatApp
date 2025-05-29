43881ca4832f3336c203b985fd518e5a56fa49b7
MarioTonchev <mariotonchev500@gmail.com>
2025-05-29 20:30:27 +0300

    adding helper functions file

---
+++ ChatApp/HelperFunctions.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+#include "HelperFunctions.h"
+
+using namespace std;
+
+void clearConsole()
+{
+	cout << "\033[;H";
+	cout << "\033[J";
+}
+
+void printActions(const MyString& userType) {
+	cout << "You are able to use the following commands:" << endl << endl;
+
+	if (userType == "Admin")
+	{
+		cout << "delete-user <username>" << endl;
+		cout << "delete-group <chatId>" << endl;
+		cout << "view-all-chats" << endl << endl;
+	}
+
+	cout << "view-chats" << endl;
+	cout << "select-chat <chatId>" << endl;
+	cout << "create-group <groupName> <username1> <username2> ...." << endl;
+	cout << "leave-group <chatId>" << endl;
+	cout << "add-to-group <chatId> <username>" << endl;
+	cout << "kick-from-group <chatId> <username>" << endl;
+	cout << "set-group-admin <chatId> <username>" << endl;
+	cout << "group-stats <chatId>" << endl << endl;
+
+	cout << "logout" << endl;
+	cout << "quit" << endl;
+}
---
+++ ChatApp/HelperFunctions.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "MyString.h"
+
+void clearConsole();
+void printActions(const MyString& userType);
