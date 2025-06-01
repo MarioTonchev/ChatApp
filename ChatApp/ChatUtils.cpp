32b403a597cf7f55b9b3ee0c60f97bcaf9a67113
MarioTonchev <mariotonchev500@gmail.com>
2025-06-02 00:48:28 +0300

    adding chat related helper functions files

---
+++ ChatApp/ChatUtils.cpp
@@ -0,0 +1,57 @@
+#include "ChatUtils.h"
+#include "Chat.h"
+#include "GroupChat.h"
+#include "IndividualChat.h"
+
+Chat* findChatById(int id, MyVector<Chat*>& chats) {
+	for (size_t i = 0; i < chats.getSize(); i++)
+	{
+		if (chats[i]->getChatId() == id)
+		{
+			return chats[i];
+		}
+	}
+
+	return nullptr;
+}
+
+Chat* findIndividualChatByUsernames(const MyString& username1, const MyString& username2, MyVector<Chat*>& chats) {
+	for (size_t i = 0; i < chats.getSize(); i++)
+	{
+		if (IndividualChat* chat = dynamic_cast<IndividualChat*>(chats[i]))
+		{
+			MyString participant1Username = chat->getParticipants()[0]->getUsername();
+			MyString participant2Username = chat->getParticipants()[1]->getUsername();
+
+			if ((participant1Username == username1 && participant2Username == username2)
+				|| (participant1Username == username2 && participant2Username == username1))
+			{
+				return chats[i];
+			}
+		}
+	}
+
+	return nullptr;
+}
+
+Chat* findGroupChatByName(const MyString& groupName, MyVector<Chat*>& chats) {
+	for (size_t i = 0; i < chats.getSize(); i++)
+	{
+		if (GroupChat* chat = dynamic_cast<GroupChat*>(chats[i]))
+		{
+			if (chat->getChatName() == groupName)
+			{
+				return chats[i];
+			}
+		}
+	}
+
+	return nullptr;
+}
+
+const MyString getChatType(Chat* chat) {
+	MyString classType = typeid(*chat).name();
+	MyString chatType = classType.split(' ')[1];
+
+	return chatType;
+}
---
+++ ChatApp/ChatUtils.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "MyString.h"
+#include "MyVector.hpp"
+#include "User.h"
+
+class Chat;
+
+Chat* findChatById(int id, MyVector<Chat*>& chats);
+Chat* findIndividualChatByUsernames(const MyString& username1, const MyString& username2, MyVector<Chat*>& chats);
+Chat* findGroupChatByName(const MyString& groupName, MyVector<Chat*>& chats);
+const MyString getChatType(Chat* chat);
