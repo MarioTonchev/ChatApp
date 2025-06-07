ea8285f0a3f69a198f30c2ff006c94e2241acf6f
MarioTonchev <mariotonchev500@gmail.com>
2025-06-07 19:09:05 +0300

    adding binary file name constants

--- ChatApp/FileConstants.cpp
+++ ChatApp/FileConstants.cpp
@@ -2,7 +2,14 @@
 
 const char* usersFile = "users.txt";
 const char* usersFileBin = "users.bin";
+
 const char* chatsFile = "chats_";
+
 const char* chatIdsFile = "chatIds.txt";
-const char* usersChatsFile = "usersChats.txt";
+const char* chatIdsFileBin = "chatIds.bin";
+
 const char* approvalListFile = "approvalList.txt";
+const char* approvalListFileBin = "approvalList.bin";
+
+const char* usersChatsFile = "usersChats.txt";
+const char* usersChatsFileBin = "usersChats.bin";
--- ChatApp/FileConstants.h
+++ ChatApp/FileConstants.h
@@ -2,7 +2,14 @@
 
 extern const char* usersFile;
 extern const char* usersFileBin;
+
 extern const char* chatsFile;
-extern const char* usersChatsFile;
+
 extern const char* chatIdsFile;
+extern const char* chatIdsFileBin;
+
 extern const char* approvalListFile;
+extern const char* approvalListFileBin;
+
+extern const char* usersChatsFile;
+extern const char* usersChatsFileBin;
