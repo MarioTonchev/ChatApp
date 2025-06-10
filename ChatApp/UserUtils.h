ca40aa1ed92bdc1f95204f1458fedeedf50212cb
MarioTonchev <mariotonchev500@gmail.com>
2025-06-10 14:36:17 +0300

    deleting obsolete function

--- ChatApp/UserUtils.h
+++ ChatApp/UserUtils.h
@@ -9,7 +9,6 @@ class User;
 
 User* findUser(const MyString& username, const MyVector<User*>& users);
 const MyString getUserType(User*& user);
-bool setCurrentUser(const MyString& username, const MyString& password, MyVector<User*>& users, User*& loggedUser);
 void createRegularAccount(const MyString& username, const MyString& password, MyVector<User*>& users, FileHandler* fileHandler);
 void createAdminAccout(const MyString& username, const MyString& password, MyVector<User*>& users, FileHandler* fileHandler);
 void login(const MyString& username, const MyString& password, MyVector<User*>& users, User*& loggedUser, MyString& userType, FileHandler* fileHandler);
