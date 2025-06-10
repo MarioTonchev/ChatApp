a9399b30a0be8adb2189f0b6f6481fd4bc08972c
MarioTonchev <mariotonchev500@gmail.com>
2025-06-10 14:49:58 +0300

    adding try-catch block

--- ChatApp/ChatApp.cpp
+++ ChatApp/ChatApp.cpp
@@ -1,7 +1,20 @@
 ﻿#include "Application.h"
+#include <stdexcept>
 
 int main()
 {
-	Application app("txt"); //choose "txt" or "bin"
-	app.run();
+	try
+	{
+		Application app("bin"); //choose "txt" or "bin"
+		app.run();
+	}
+	catch (const std::invalid_argument& e) {
+		cout << e.what();
+	}
+	catch (const std::runtime_error& e) {
+		cout << e.what();
+	}
+	catch (const std::exception& e) {
+		cout << e.what();
+	}
 }
