d17e98175e488e09dbe0c13453f0de6f54d78a6e
MarioTonchev <mariotonchev500@gmail.com>
2025-06-10 14:44:15 +0300

    changing find chat functions return type

--- ChatApp/ChatUtils.cpp
+++ ChatApp/ChatUtils.cpp
@@ -19,7 +19,22 @@ Chat* findChatById(int id, MyVector<Chat*>& chats) {
 	return nullptr;
 }
 
-Chat* findIndividualChatByUsernames(const MyString& username1, const MyString& username2, MyVector<Chat*>& chats) {
+GroupChat* findGroupChatById(int id, MyVector<Chat*>& chats) {
+	for (size_t i = 0; i < chats.getSize(); i++)
+	{
+		if (GroupChat* groupChat = dynamic_cast<GroupChat*>(chats[i]))
+		{
+			if (groupChat->getChatId() == id)
+			{
+				return groupChat;
+			}
+		}
+	}
+
+	return nullptr;
+}
+
+IndividualChat* findIndividualChatByUsernames(const MyString& username1, const MyString& username2, MyVector<Chat*>& chats) {
 	for (size_t i = 0; i < chats.getSize(); i++)
 	{
 		if (IndividualChat* chat = dynamic_cast<IndividualChat*>(chats[i]))
@@ -30,7 +45,7 @@ Chat* findIndividualChatByUsernames(const MyString& username1, const MyString& u
 			if ((participant1Username == username1 && participant2Username == username2)
 				|| (participant1Username == username2 && participant2Username == username1))
 			{
-				return chats[i];
+				return chat;
 			}
 		}
 	}
@@ -38,14 +53,14 @@ Chat* findIndividualChatByUsernames(const MyString& username1, const MyString& u
 	return nullptr;
 }
 
-Chat* findGroupChatByName(const MyString& groupName, MyVector<Chat*>& chats) {
+GroupChat* findGroupChatByName(const MyString& groupName, MyVector<Chat*>& chats) {
 	for (size_t i = 0; i < chats.getSize(); i++)
 	{
 		if (GroupChat* chat = dynamic_cast<GroupChat*>(chats[i]))
 		{
 			if (chat->getChatName() == groupName)
 			{
-				return chats[i];
+				return chat;
 			}
 		}
 	}
@@ -69,6 +84,12 @@ void deleteChat(Chat* chat, MyVector<Chat*>& chats, FileHandler* fileHandler) {
 	fileHandler->deleteChatIdFromFile(chat->getChatId());
 	fileHandler->deleteChatFromFile(chat->getChatId());
 
+	for (size_t i = 0; i < chat->getParticipants().getSize(); i++)
+	{
+		chat->getParticipants()[i]->removeChat(chat);
+		fileHandler->deleteUserChatRelation(chat->getParticipants()[i]->getUsername(), chat->getChatId());
+	}
+
 	for (size_t i = 0; i < chats.getSize(); i++)
 	{
 		if (chats[i]->getChatId() == chat->getChatId())
@@ -77,4 +98,6 @@ void deleteChat(Chat* chat, MyVector<Chat*>& chats, FileHandler* fileHandler) {
 			break;
 		}
 	}
+
+	delete chat;
 }
--- ChatApp/ChatUtils.h
+++ ChatApp/ChatUtils.h
@@ -8,7 +8,8 @@
 class Chat;
 
 Chat* findChatById(int id, MyVector<Chat*>& chats);
-Chat* findIndividualChatByUsernames(const MyString& username1, const MyString& username2, MyVector<Chat*>& chats);
-Chat* findGroupChatByName(const MyString& groupName, MyVector<Chat*>& chats);
+GroupChat* findGroupChatById(int id, MyVector<Chat*>& chats);
+IndividualChat* findIndividualChatByUsernames(const MyString& username1, const MyString& username2, MyVector<Chat*>& chats);
+GroupChat* findGroupChatByName(const MyString& groupName, MyVector<Chat*>& chats);
 const MyString getChatType(Chat* chat);
 void deleteChat(Chat* chat, MyVector<Chat*>& chats, FileHandler* fileHandler);
