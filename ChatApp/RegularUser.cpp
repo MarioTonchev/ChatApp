0b87444319ae7e7003d77798c0ca22ee6a9bef03
MarioTonchev <mariotonchev500@gmail.com>
2025-06-10 14:48:54 +0300

    implementing print actions func

--- ChatApp/RegularUser.cpp
+++ ChatApp/RegularUser.cpp
@@ -1,5 +1,27 @@
+#include <iostream>
+
 #include "RegularUser.h"
+
+using namespace std;
 
 RegularUser::RegularUser(const MyString& username, const MyString& password, FileHandler* fileHandler) : User(username, password, fileHandler) {
 
+}
+
+void RegularUser::printActions() const {
+	cout << "view-chats" << endl;
+	cout << "select-chat <chatId>" << endl;
+	cout << "create-individual-chat <username>" << endl;
+	cout << "create-group <groupName> <username1> <username2> ...." << endl;
+	cout << "leave-group <chatId>" << endl;
+	cout << "add-to-group <chatId> <username>" << endl;
+	cout << "kick-from-group <chatId> <username>" << endl;
+	cout << "set-group-admin <chatId> <username>" << endl;
+	cout << "group-stats <chatId>" << endl << endl;
+	cout << "toggle-group-add-approval <chatId>" << endl;
+	cout << "view-group-approval-list <chatId>" << endl;
+	cout << "approve <chatId> <username>" << endl << endl;
+
+	cout << "logout" << endl;
+	cout << "quit" << endl;
 }
